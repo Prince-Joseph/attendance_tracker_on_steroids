45e04de931af3e68fe1cdc225b65bcbc1aa19aa6
S E Prince Joseph <sindala.prince@gmail.com>
2022-08-08 15:03:46 +0100

    linebreak in admin.py

--- at_backend/attendance/admin.py
+++ at_backend/attendance/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import  Classroom, DateWS, Department,Subject,SubjectTeacher,Timetable,Teacher,Student,Attendance
+from .models import  Classroom, DateWS,
+                    Department,Subject,SubjectTeacher,Timetable,Teacher,Student,Attendance
 
 admin.site.register(Classroom)
 admin.site.register(DateWS)
